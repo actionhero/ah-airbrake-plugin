6acd7cb2dfd96b4af04c83f6f24458c67e6c2607
Evan Tahler <evan@taskrabbit.com>
2014-05-21 13:10:20 -0700

    fix load order

--- initializers/airbrake.js
+++ initializers/airbrake.js
@@ -4,8 +4,8 @@ exports.airbrake = function(api, next){
 
   api.airbrake.configure = function(){
     var airbrakePrototype = require('airbrake');
-    api.airbrake.client = airbrakePrototype.createClient(api.airbrake.token);
     api.airbrake.token  = api.config.airbrake.token;
+    api.airbrake.client = airbrakePrototype.createClient(api.airbrake.token);
     api.airbrake.client.handleExceptions(); // catch global uncaught errors
     // api.airbrake.client.developmentEnvironments = []; // don't report in various NODE_ENVs
   }
